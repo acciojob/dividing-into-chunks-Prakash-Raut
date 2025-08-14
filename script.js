6cb24952c21e4c1302e15ec7bc0bddc8f60421c3
Prakash Raut <prakash_raut@outlook.com>
2025-08-14 11:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const chunks = [];
+	let current = [];
+	let sum = 0;
+
+	for (const num of arr) {
+		if (num + sum <= n) {
+			current.push(num);
+			sum += num;
+		}
+		else {
+			chunks.push(current);
+		    current = [num]; 
+		    sum = num;
+		}
+	}
+
+	// if current is not empty push current to chunks
+	 if (current.length) chunks.push(current);
+	return chunks;
 };
 
 const n = prompt("Enter n: ");
